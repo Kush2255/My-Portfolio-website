f85887ad9fdb60f47a616a6ca780ecaa1eb28ca8
Kushwanth Rasala <155243610+Kush2255@users.noreply.github.com>
2025-01-16 18:53:02 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {  
+    function showMoreProjects() {
+        var hiddenProjects = document.querySelectorAll('.more-projects');  
+        hiddenProjects.forEach(function (project) {
+            project.style.display = 'block';
+        });    
+        var seeMoreButton = document.querySelector('.btn');
+        if (seeMoreButton) {
+            seeMoreButton.style.display = 'none';
+        }
+    }
+    var seeMoreButton = document.querySelector('.btn');
+    if (seeMoreButton) {
+        seeMoreButton.addEventListener('click', showMoreProjects);
+    }
+});
